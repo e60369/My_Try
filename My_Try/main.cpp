33c7a755a130e22d48554a0a633bb2fdeaa66871
R. Murphy <e60369@gmail.com>
2017-10-23 13:16:57 -0400

    Initial Commit

---
+++ My_Try/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  My_Try
+//
+//  Created by Richard T. Murphy on 10/23/17.
+//  Copyright © 2017 Richard T. Murphy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
